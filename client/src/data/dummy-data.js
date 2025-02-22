0326d83b717004d97d73cfdcb9d6dee194857515
ikealmighty <isaacyates7@gmail.com>
2025-02-22 14:33:33 -0600

    dummyData update

--- client/src/data/dummy-data.js
+++ client/src/data/dummy-data.js
@@ -1,10 +1,17 @@
 // This file will parse a csv file and make dummy data available to the app from it
 
-const dummyData = { collections: {} }
+const dummyData = { collections: {} };
+const tabIds = {
+    "strategyTools": '0',
+    "studentAccounts": '1609703130'
+};
 
 const SHEET_ID = "1ik_6TgaKqsNULRVBq_PPTj8VZ3BAck8GqeL1iXgqZsI"; // Replace with your Sheet ID
 
-async function populateDummyDataFromTab(sheet_tab_id) {
+async function populateCollection(collectionName) {
+    const sheet_tab_id = tabIds[collectionName];
+    dummyData.collections[collectionName] = {}
+
     const url = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:csv&gid=${sheet_tab_id}`;
 
     function formatQuotes(value) {
@@ -18,28 +25,24 @@ async function populateDummyDataFromTab(sheet_tab_id) {
         return v;
     }
 
+    const fields = {}
+
     return fetch(url)
         .then(response => response.text())
         .then(csv => {
             const rows = csv.split("\n").map(row => row.split(","));
-
-            const collectionsIndexesToName = {};
-            rows.forEach((row) => {
-                row.forEach((value, index) => {
-                    let collectionName = collectionsIndexesToName[index];
-
-                    if (value.includes("*collection")) {
-                        collectionName = formatQuotes(value.split('*')[0]);
-                        dummyData.collections[collectionName] = [];
-                        collectionsIndexesToName[index] = collectionName;
-                    } else {
-                        // if it's not a collection label, and it's in a collection column, 
-                        // add it to the collection!
-
-                        if (value.trim() !== "\"\"" && dummyData.collections[collectionName]) {
-
-                            dummyData.collections[collectionName].push(value.trim().slice(1, value.length - 1));
-                        }
+            rows.forEach((row, rowIndex) => {
+                row.forEach((value, colIndex) => {
+                    if (value.includes("*key")) {
+                        // add the key to fields lookup
+                        let key = value.trim().split("*")[0].slice(1);
+                        fields[colIndex] = key;
+                    }
+                    else if (fields[colIndex] && value !== '\"\"') {
+                        const key = fields[colIndex];
+                        let currentDoc = dummyData.collections[collectionName][rowIndex];
+                        if (!currentDoc) dummyData.collections[collectionName][rowIndex] = {}
+                        dummyData.collections[collectionName][rowIndex][key] = value;
                     }
                 })
             })
@@ -47,9 +50,7 @@ async function populateDummyDataFromTab(sheet_tab_id) {
         .catch(error => console.error("Error fetching sheet:", error));
 }
 
-
-
-populateDummyDataFromTab("1609703130").then(() => console.log('dummy data populated with sheet 1'));
-populateDummyDataFromTab("0").then(() => console.log('dummy data populated with sheet 2'));
+populateCollection("strategyTools").then(() => console.log(JSON.stringify(dummyData, null, 3)));
+populateCollection("studentAccounts").then(() => console.log(JSON.stringify(dummyData, null, 3)));
 
 export default dummyData;
