d2131dd2e9a61427e4bf0d1e570024417088c10e
ikealmighty <isaacyates7@gmail.com>
2025-02-22 11:24:34 -0600

    added skeleton file for student profile page

---
+++ client/src/data/dummy-data.js
@@ -0,0 +1,7 @@
+// This file will parse a csv file and make dummy data available to the app from it
+
+// TODO: parse csv file from our public dummy data sheet file
+
+const dummyData = {}
+
+export default dummyData;
